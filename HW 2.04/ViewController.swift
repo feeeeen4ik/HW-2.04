454a2116c98a2d9ce1a521fd0a6f5f11152004c0
feeeeeen4ik <feeeeen4ik1994@gmail.com>
2024-10-30 19:47:20 +0500

    did homework

--- HW 2.04/ViewController.swift
+++ HW 2.04/ViewController.swift
@@ -7,11 +7,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var viewOutlet: UIView!
+    @IBOutlet var redSliderOutlet: UISlider!
+    @IBOutlet var greenSliderOutlet: UISlider!
+    @IBOutlet var blueSliderOutlet: UISlider!
+    @IBOutlet var redLabelOutlet: UILabel!
+    @IBOutlet var greenLabelOutlet: UILabel!
+    @IBOutlet var blueLabelOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewOutlet.layer.cornerRadius = 10
+        redLabelOutlet.text = String(format: "%5.2f", redSliderOutlet.value)
+        greenLabelOutlet.text = String(format: "%5.2f", greenSliderOutlet.value)
+        blueLabelOutlet.text = String(format: "%5.2f", blueSliderOutlet.value)
+        changeColorOnView()
+        setupSliders()
+    }
+    
+    @IBAction func redSliderAction () {
+        redLabelOutlet.text = String(format: "%5.2f", redSliderOutlet.value)
+        changeColorOnView()
+    }
+
+    @IBAction func greenSliderAction () {
+        greenLabelOutlet.text = String(format: "%5.2f", greenSliderOutlet.value)
+        changeColorOnView()
+    }
+    
+    @IBAction func blueSliderAction () {
+        blueLabelOutlet.text = String(format: "%5.2f", blueSliderOutlet.value)
+        changeColorOnView()
+    }
+    
+    private func setupSliders() {
+        redSliderOutlet.minimumValue = 0
+        redSliderOutlet.maximumValue = 1
+        redSliderOutlet.value = 0.5
+        redSliderOutlet.minimumTrackTintColor = .red
+        
+        greenSliderOutlet.minimumValue = 0
+        greenSliderOutlet.maximumValue = 1
+        greenSliderOutlet.value = 0.5
+        greenSliderOutlet.minimumTrackTintColor = .green
+        
+        blueSliderOutlet.minimumValue = 0
+        blueSliderOutlet.maximumValue = 1
+        blueSliderOutlet.value = 0.5
+        blueSliderOutlet.minimumTrackTintColor = .blue
+        
+    }
+    
+    private func changeColorOnView() {
+        viewOutlet.backgroundColor = UIColor(
+            red: CGFloat(redSliderOutlet.value),
+            green: CGFloat(greenSliderOutlet.value),
+            blue: CGFloat(blueSliderOutlet.value),
+            alpha: 1
+        )
     }
 
 
